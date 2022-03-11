a4551d7544b01217206027e0f72df3d8a9f3cbe4
mirai110 <65322807+mirai110@users.noreply.github.com>
2022-03-11 16:09:13 +0900

    検索候補を表示できるようにした。

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_photo_tag/main_app_bar.dart';
-import 'package:flutter_application_photo_tag/select_photo/select_photo.dart';
 
 import 'package:flutter_application_photo_tag/tag_feature/tag.dart';
 import 'package:flutter_application_photo_tag/tag_library/library_page.dart';
--- lib/search/search_view.dart
+++ lib/search/search_view.dart
@@ -16,65 +16,75 @@ class _SearchViewState extends State<SearchView> {
   TextEditingController controller = TextEditingController();
   List<Tag> tags = Boxes.getTags().values.toList();
   List<int> searchIndexList = [];
+  // List<Tag>
 
   @override
   void initState() {
     super.initState();
+    controller.addListener(searchWordContains);
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is removed from the
+    // widget tree.
+    controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const MainAppBar(),
-      body: Column(
-        children: [
-          searchTextContainer(),
-          searchListView(),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text("$searchIndexList"),
+            searchTextContainer(searchTextField()),
+            suggestionContainer(suggestionListView()),
+            searchListView(),
+          ],
+        ),
       ),
     );
   }
 
-  Widget searchTextContainer() {
+  Widget searchTextContainer(TextField textField) {
     return Container(
-      margin: const EdgeInsets.all(8),
+      margin: const EdgeInsets.only(
+        top: 8,
+        left: 8,
+        right: 8,
+        // bottom: 8,
+      ),
       decoration: BoxDecoration(
         color: Colors.grey.withOpacity(0.2),
         // border: Border.all(color: Colors.black),
         borderRadius: BorderRadius.circular(10),
       ),
-      child: searchTextField(),
+      child: textField,
     );
   }
 
-  Widget searchListView() {
-    return GridView.builder(
-        shrinkWrap: true,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 20.0, // 縦スペース
-          mainAxisSpacing: 20.0, //横スペース
-        ),
-        padding: const EdgeInsets.all(4),
-        itemCount: searchIndexList.length,
-        itemBuilder: (context, index) {
-          index = searchIndexList[index];
-          return TagWidget(tag: tags[index]);
-        });
+  void searchWordContains() {
+    // print(searchIndexList);
+    setState(() {
+      searchIndexList = [];
+      if (controller.text.isNotEmpty) {
+        for (int i = 0; i < tags.length; i++) {
+          print(controller.text);
+          if (tags[i].tagName.contains(controller.text)) {
+            searchIndexList.add(i);
+            print(searchIndexList);
+          }
+        }
+      }
+    });
   }
 
-  Widget searchTextField() {
+  TextField searchTextField() {
     return TextField(
-      onChanged: (String s) {
-        setState(() {
-          searchIndexList = [];
-          for (int i = 0; i < tags.length; i++) {
-            if (tags[i].tagName.contains(s)) {
-              searchIndexList.add(i);
-            }
-          }
-        });
-      },
+      controller: controller,
 
       autofocus: true,
       style: const TextStyle(
@@ -102,5 +112,65 @@ class _SearchViewState extends State<SearchView> {
         ),
       ),
     );
+  }
+
+  Widget suggestionContainer(Widget suggestionListView) {
+    //このif文がないと、Containerのborderが表示してしまう。
+    if (searchIndexList.isNotEmpty) {
+      return Container(
+          margin: const EdgeInsets.only(
+            left: 8,
+            right: 8,
+            bottom: 8,
+          ),
+          decoration: BoxDecoration(
+            // color: Colors.grey.withOpacity(0.2),
+            border: Border.all(color: Colors.grey.withOpacity(0.3)),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: suggestionListView);
+    } else {
+      return Container();
+    }
+  }
+
+  Widget suggestionListView() {
+    return ListView.builder(
+        physics: const NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        itemCount: searchIndexList.length,
+        itemBuilder: (context, index) {
+          index = searchIndexList[index];
+          return ListTile(
+            onTap: () {
+              controller.text = tags[index].tagName + " ";
+              controller.selection = TextSelection.fromPosition(
+                  TextPosition(offset: controller.text.length));
+              // controller.addListener(tags[index].tagName);
+            },
+            trailing: const Icon(
+              Icons.north_west,
+              color: Colors.grey,
+            ),
+            title: Text(tags[index].tagName),
+            subtitle: Text(controller.text),
+          );
+        });
+  }
+
+  Widget searchListView() {
+    return GridView.builder(
+        shrinkWrap: true,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 20.0, // 縦スペース
+          mainAxisSpacing: 20.0, //横スペース
+        ),
+        padding: const EdgeInsets.all(4),
+        itemCount: searchIndexList.length,
+        itemBuilder: (context, index) {
+          index = searchIndexList[index];
+          return TagWidget(tag: tags[index]);
+        });
   }
 }
