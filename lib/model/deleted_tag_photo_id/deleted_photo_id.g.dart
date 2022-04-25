fefbe37538eab7880ec0a9714ef60852dbc7cc39
mirai110 <65322807+mirai110@users.noreply.github.com>
2022-04-25 21:00:25 +0900

    Hiveを使って、deleted_idのデータベースを作成

---
+++ lib/model/deleted_tag_photo_id/deleted_photo_id.g.dart
@@ -0,0 +1,44 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'deleted_photo_id.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class DeletedPhotoIdAdapter extends TypeAdapter<DeletedPhotoId> {
+  @override
+  final int typeId = 2;
+
+  @override
+  DeletedPhotoId read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return DeletedPhotoId(
+      photoId: fields[10] as String,
+      deletedTime: fields[11] as DateTime,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, DeletedPhotoId obj) {
+    writer
+      ..writeByte(2)
+      ..writeByte(10)
+      ..write(obj.photoId)
+      ..writeByte(11)
+      ..write(obj.deletedTime);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is DeletedPhotoIdAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
