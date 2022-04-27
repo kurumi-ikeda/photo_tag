441881d4f0a52ddb5badad2149faad6cd1570e47
mirai110 <65322807+mirai110@users.noreply.github.com>
2022-04-27 22:55:11 +0900

    nullが発生した、assetEntityのIdをHiveで保存する所まで出来た。

--- lib/model/deleted_tag_photo_id/deleted_photo_id.dart
+++ lib/model/deleted_tag_photo_id/deleted_photo_id.dart
@@ -1,4 +1,6 @@
 import 'package:hive/hive.dart';
+
+import '../boxes.dart';
 part 'deleted_photo_id.g.dart';
 
 ///Tagクラスに保存している [photoId] が、[AssetEntity.fromId()] をしたときにnullだったものを30日間保存しておくクラス
@@ -10,4 +12,18 @@ class DeletedPhotoId {
   late String photoId;
   @HiveField(11)
   late DateTime deletedTime;
+
+  static nullRecordPhotoId(String photoId) {
+    final DeletedPhotoId recordPhotoId =
+        DeletedPhotoId(deletedTime: DateTime.now(), photoId: photoId);
+    Boxes.getDeletedPhotoIds().add(recordPhotoId);
+    for (var deletedPhotoId in Boxes.getDeletedPhotoIds().values.toList()) {
+      print(deletedPhotoId.photoId);
+      print(deletedPhotoId.deletedTime);
+    }
+    //     for (var tag in box.values.toList()) {
+    //   print('tag name: ${tag.tagName}');
+    //   print('photoIdList: ${tag.photoIdList}');
+    // }
+  }
 }
