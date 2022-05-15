5c50bfd7fcaa835c7ca084ba0f086ca6e3c1d195
mirai110 <65322807+mirai110@users.noreply.github.com>
2022-05-16 02:08:10 +0900

    サンプルの完成

--- lib/model/deleted_tag_photo_id/box_deleted_photo_id.dart
+++ lib/model/deleted_tag_photo_id/box_deleted_photo_id.dart
@@ -1,43 +1,52 @@
+import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:uuid/uuid.dart';
 
 import 'deleted_photo_id.dart';
 
 class BoxDeletedPhotoId {
-  final Box<DeletedPhotoId> boxDeletedPhotoIds =
+  final Box<DeletedPhotoId> _boxDeletedPhotoIds =
       Hive.box<DeletedPhotoId>('deletedPhotoIds');
 
-  nullRecordPhotoId(String photoId) {
+  nullRecordPhotoId(final String photoId) {
     final DeletedPhotoId recordPhotoId = DeletedPhotoId(
-        deletedTime: DateTime.now(), photoId: photoId, uuid: Uuid().v4());
+        deletedTime: DateTime.now(), photoId: photoId, uuid: const Uuid().v4());
 
-    boxDeletedPhotoIds.add(recordPhotoId);
-    for (var deletedPhotoId in boxDeletedPhotoIds.values.toList()) {
+    _boxDeletedPhotoIds.add(recordPhotoId);
+    for (var deletedPhotoId in _boxDeletedPhotoIds.values.toList()) {
       print(deletedPhotoId.photoId);
       print(deletedPhotoId.deletedTime);
     }
   }
 
-  deletedPhotoId() {
-    int testCount = 0;
-    List<DeletedPhotoId> deletedPhotoIds = boxDeletedPhotoIds.values.toList();
+  periodDeletedPhotoId() {
+    final List<DeletedPhotoId> deletedPhotoIds =
+        _boxDeletedPhotoIds.values.toList();
 
     // (DeletedPhotoIds) => {};
-    print("sss ${boxDeletedPhotoIds.values.length}");
+    print("sss ${_boxDeletedPhotoIds.values.length}");
     for (DeletedPhotoId deletedPhotoId in deletedPhotoIds) {
-      testCount++;
       print(deletedPhotoId.deletedTime.difference(DateTime.now()).inDays);
-      bool timeLimit =
+      // TODO: あとで、1を31にすること
+      final bool timeLimit =
           DateTime.now().difference(deletedPhotoId.deletedTime).inDays.abs() >
               1;
       print(timeLimit);
-      if (!timeLimit) {
-        return;
-      }
 
-      print("object");
+      //早期return
+      if (!timeLimit) {
+        continue;
+      }
+      _deletedPhotoId(deletedPhotoId);
 
       // if (deletedPhotoId.deletedTime.difference(DateTime.now()).inDays >= 30) {}
     }
   }
+
+  _deletedPhotoId(final DeletedPhotoId deletedPhotoId) async {
+    await _boxDeletedPhotoIds.delete(deletedPhotoId.uuid);
+  }
+  // deleteTag(Tag tag) async {
+  //   await _boxTag.delete(tag.key);
+  // }
 }
