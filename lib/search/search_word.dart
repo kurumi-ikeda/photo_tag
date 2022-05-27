be2eefca870148354c766736c4c9704fa0027c47
mirai110 <65322807+mirai110@users.noreply.github.com>
2022-05-27 20:26:17 +0900

    すでに、検索ボックスに入力されているtagNameはサジェストに表示をさせなくした

---
+++ lib/search/search_word.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_photo_tag/model/tag/tag.dart';
+
+//SearchPageの検索するロジックを分離させたclass
+class SearchWord {
+  Set<Tag> searchWordContains(
+    final TextEditingController controller,
+    final List<Tag> tags,
+  ) {
+    if (controller.text.isEmpty) {
+      final Set<Tag> setTag = {};
+      return setTag;
+    }
+
+    //空白文字区切り
+    final List<String> splitSearchWords = controller.text.split(RegExp(r'\s'));
+    //初期化
+    final Set<Tag> searchTagList = {};
+
+    if (!controller.text.isNotEmpty) {
+      final Set<Tag> setTag = {};
+      return setTag;
+    }
+
+    for (String word in splitSearchWords) {
+      for (int i = 0; i < tags.length; i++) {
+        // if (searchTagList.map((e) => e.tagName).toList().contains(word)) {
+
+        // }
+        if (tags[i].tagName.contains(word) && word.isNotEmpty) {
+          searchTagList.add(tags[i]);
+        }
+
+        // if (tags[i].tagName.contains(controller.text)) {
+        //   searchTagList.add(tags[i]);
+        // }
+      }
+    }
+
+    return searchTagList;
+  }
+}
